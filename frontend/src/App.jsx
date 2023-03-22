95c361dccc3aea9ce09c09929bca11c3b1eace96
Victor Alves <victormssalves@outlook.com>
2023-03-22 20:44:42 -0300

    Login feito

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
 import FormProduct from './components/formProducts/FormProducts';
 import FormUser from './components/formUsers/FormUsers';
 import Login from './components/login/Login';
+import Home from './components/home/Home';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <div>
-      <Login/>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/Home">Home</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Login />}>
+          </Route>
+          <Route path="/login"  element={<Home />}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,4 +1,28 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 export default function Login() {
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useNavigate();
+    
+    async function login() {
+        try {
+          const response = await axios.post('http://localhost:3000/login', {
+            user,
+            password
+          });
+          const data = response.data;
+          if (data.success) {
+            history.push('/Home'); // substitua '/nextpage' pelo caminho do formulário desejado
+          } else {
+            alert(data.message);
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    
     return (
     <>
       <section className="bg-gray-50 ">
@@ -11,10 +35,10 @@ export default function Login() {
               <h1 className="text-xl font-semibold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                   Entre em sua conta
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form className="space-y-4 md:space-y-6" action="POST">
                   <div>
                       
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Usuário" required=""></input>
+                      <input type="text" name="user" id="user" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Usuário" required=""></input>
                   </div>
                   <div>
                       
@@ -31,7 +55,7 @@ export default function Login() {
                       </div>
                       <a href="www.google.com" className="text-sm font-medium text-primary-600 hover:underline">Esqueceu a senha?</a>
                   </div>
-                  <button type="button" className="transition duration-200 bg-pink-500 hover:bg-pink-600 focus:bg-pink-700 focus:shadow-sm focus:ring-4 focus:ring-pink-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
+                  <button type="submit" onClick={() => login()} className="transition duration-200 bg-pink-500 hover:bg-pink-600 focus:bg-pink-700 focus:shadow-sm focus:ring-4 focus:ring-pink-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
                       <span className="inline-block mr-2">Entrar</span>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
