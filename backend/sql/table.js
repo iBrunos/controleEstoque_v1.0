af4dbb66dbf6699e29598d8564e64741373a2fac
Bruno Seixas <102114046+iBrunos@users.noreply.github.com>
2023-03-25 19:56:34 -0300

    a

--- backend/sql/table.js
+++ backend/sql/table.js
@@ -51,7 +51,7 @@ createEntrys() {
 }
 createExits() {
   const sql =
-    "CREATE TABLE IF NOT EXISTS entrys (id int NOT NULL AUTO_INCREMENT, product varchar(30) NOT NULL, price varchar(20) NOT NULL, brand varchar(20) NOT NULL, description text NOT NULL, amount int NOT NULL, inserted_by varchar(30) ,PRIMARY KEY(id))";
+    "CREATE TABLE IF NOT EXISTS exits (id int NOT NULL AUTO_INCREMENT, product varchar(30) NOT NULL, price varchar(20) NOT NULL, brand varchar(20) NOT NULL, description text NOT NULL, amount int NOT NULL, inserted_by varchar(30) ,PRIMARY KEY(id))";
 
   this.connection.query(sql, (erro) => {
     if (erro) {
